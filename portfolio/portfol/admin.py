5c66a9c9f015980b360313452e95b3662e78875a
Google Vormodial <70081718+John-Divine@users.noreply.github.com>
2021-04-04 17:20:38 +0700

    Portfolio
    My porfolio project

---
+++ portfolio/portfol/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(PersonalInfo)
+admin.site.register(Contact)
+admin.site.register(Experience)
+admin.site.register(competence)
+admin.site.register(Education)
+admin.site.register(Project)
---
+++ portfolio/portfol/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PortfolConfig(AppConfig):
+    name = 'portfol'
